dc8570c657bdb87611a712618f97cf154c491175
kjw0612 <kjw0612@gmail.com>
2015-10-04 21:44:22 +0900

    CUDNN v3

--- setup.m
+++ setup.m
@@ -1,9 +1,9 @@
 % This setup is tested under Ubuntu 14.04. CUDNN needs to be copied into local directory
 addpath matlab
-% download and extract CUDNN v2 rc2
+% download and extract CUDNN v3
 if ~exist('./local', 'dir')
-    fprintf('Download CUDNN v2 rc2\n');
-    unzip('https://www.dropbox.com/s/s7wlgwt792z11ye/local.zip?dl=1');  
+    fprintf('Download CUDNN v3\n');
+    unzip('https://www.dropbox.com/s/c9hxe1zhvhys4fz/local.zip?dl=1');  
     fprintf('CUDNN sucessfully downloaded\n');
 end
 vl_compilenn('enableGPU', 1, 'cudaRoot', '/usr/local/cuda', 'cudaMethod', 'nvcc', 'enableCudnn', 1, 'cudnnRoot', 'local/');
